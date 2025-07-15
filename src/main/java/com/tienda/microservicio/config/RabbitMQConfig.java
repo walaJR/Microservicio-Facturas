77dda6a191db75d968f86787ddd35ad22a94911a
Sebastián Olave Y. <165727846+walaJR@users.noreply.github.com>
2025-07-14 20:18:15 -0400

    Update RabbitMQConfig.java

--- src/main/java/com/tienda/microservicio/config/RabbitMQConfig.java
+++ src/main/java/com/tienda/microservicio/config/RabbitMQConfig.java
@@ -17,6 +17,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
 
+
 @Configuration
 @EnableRabbit
 public class RabbitMQConfig {
